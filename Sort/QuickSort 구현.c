fcdab48c83f40c0be5ddd873352f9097fbdc6772
Twince <Twince-Dev@users.noreply.github.com>
2020-11-01 21:24:05 +0900

    Update QuickSort 구현.c

--- Sort/QuickSort 구현.c
+++ Sort/QuickSort 구현.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
-int swap(int inputA, int inputB){
+int swap(int *inputA, int *inputB){
 	int temp;
 	
-	temp = inputA;
-	inputA = inputB;
-	inputB = temp;
+	temp = *inputA;
+	*inputA = *inputB;
+	*inputB = temp;
 }
 
 void main(){
-	int quickSort[5] = {5, 4, 2, 1, 7};
-	int pivot, i, k;
+	int quickSort[5] = {5, 6, 7, 1, 2};
+	int pivot, i, j;
 	
-	for(i=0; i<5; i++) printf("%d ,", i, quickSort[i]);
-	swap(quickSort[0], quickSort[4]);
-	printf("\n\n");
-	for(i=0; i<5; i++) printf("%d ,", i, quickSort[i]);
+	for(i=0; i<5; i++) printf("%d ,",quickSort[i]);
+	printf("\b ");
+	pivot = j = 0;
+	for(i=1; i<5; i++){
+		if(quickSort[pivot] > quickSort[i]){
+			j++;
+			swap(&quickSort[j], &quickSort[i]);
+		}
+		if(i == 4) swap(&quickSort[pivot], &quickSort[j]);
+	}
+	printf("\n--------------------------------\n");
+	for(i=0; i<5; i++) printf("%d ,",quickSort[i]);
+	printf("\b ");
+	
 	
 	
 	
