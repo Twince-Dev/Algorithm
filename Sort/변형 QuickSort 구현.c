ebc258220de913e3085163a4cd40eaae595b7062
Twince <tjsdn811@gmail.com>
2020-11-03 01:45:17 +0900

    변형 퀵소트 수정

--- Sort/변형 QuickSort 구현.c
+++ Sort/변형 QuickSort 구현.c
@@ -6,7 +6,7 @@ int left, right;
 int top, stack[arrayLen];
 	top = 0;
 
-// pivot 선택을 위한 stack 
+ //pivot 선택을 위한 stack 
 int stackPush(int pushData){
 	stack[top] = pushData;
 	top++;
@@ -26,7 +26,7 @@ int show(int lengthEnd){
 
 int swap(int *inputA, int *inputB){
 	int temp;
-	
+
 	temp = *inputA;
 	*inputA = *inputB;
 	*inputB = temp;
@@ -34,7 +34,9 @@ int swap(int *inputA, int *inputB){
 
 int Qsort(int Qleft, int Qright){
 	int pivot, i, j;
+	Qleft = stackPop();
 	pivot = j = Qleft;
+	
 	
 	for(i=Qleft + 1; i<Qright; i++){
 		if(quickSort[pivot] > quickSort[i]){
@@ -43,14 +45,16 @@ int Qsort(int Qleft, int Qright){
 		}
 		if(i == Qright-1) {
 			Qright = j;
+			stackPush(j+1);
+			printf("Qright의 값: ", Qright);
 			if(Qleft == j) Qleft++;
 			swap(&quickSort[pivot], &quickSort[j]);
 		}
 	}
-	
+
 	printf("\n--------------------------------\n");
 	show(arrayLen);
-	
+
 	printf("Qleft의 값: %d", Qleft);
 	if(Qleft == 9) return 0;
 	Qsort(Qleft, 10);
