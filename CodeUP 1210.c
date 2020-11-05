68f9fe1ead780f4062a7a01ec25254d9a6ef089b
Twince <tjsdn811@gmail.com>
2020-11-05 22:28:04 +0900

    CodeUp

--- CodeUP 1210.c
+++ CodeUP 1210.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 main()
 {
-	//1210 : 칼로리 계산하기 / Calculating Calories Using Arrangement
+	//1210 : 칼로리 계산하기 
+	// Calculating Calories Using Arrangement
 	int menu[5]={ 400, 340, 170, 100, 70};
 	int input[2];
 	
