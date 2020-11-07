946feb61b8fe5f19bda7f3633ac38c7ea3441d31
Twince <tjsdn811@gmail.com>
2020-11-08 00:05:47 +0900

    Boj 2751

--- Boj 2751.cpp
+++ Boj 2751.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
+#include <stdio.h>
 #include <algorithm>
 using namespace std;
 
 int main(int argc, const char * argv[]){
+	//시간 초과로 인한 c 문법 출력사용 
 	int input, value ,i;
 	
-	cin >> input;
+	scanf("%d", &input);
 	
 	int array[input];
 	
 	for(i=0; i<input; i++){
-		cin >> value;
+		scanf("%d", &value);
 		array[i]= value;
 	}
 	
 	sort(array, array + input );
-	for(i=0; i<input; i++) cout << array[i] << endl;
-
-	
-	
-	
+	for(i=0; i<input; i++) printf("%d\n", array[i]);
 }
