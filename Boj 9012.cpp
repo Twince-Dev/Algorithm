2961ac73825d727cb1120faf33f2c84c8a5b7a5b
Twince <tjsdn811@gmail.com>
2020-11-09 20:49:03 +0900

    C++

---
+++ Boj 9012.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdio>
+#include <stack>
+//#include <string>
+using namespace std;
+
+int main(){
+	int input, i, j;
+	string array;
+	vector<int> v;
+	
+	cin >> array;
+	
+	if(array[i] == "(" ) v.push(i);
+	if(array[i+1] == ")") v.pop()
+		
+	
+	// (별 가중치를 더해 스택으로 저장
+	// LIFO
+	// stack;
+	//pop / push 
+	
+}
