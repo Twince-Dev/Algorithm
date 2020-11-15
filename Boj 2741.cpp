40e4a2cc329b4ff0cfaa678630f91bd5d8ab5a5d
Twince <tjsdn811@gmail.com>
2020-11-15 22:59:50 +0900

    C++

---
+++ Boj 2741.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+	int i, temp;
+	
+	cin >> temp;
+	for(i=1; i<=temp; i++){
+		cout << i;
+		printf("\n");
+	}
+	return 0;
+}
