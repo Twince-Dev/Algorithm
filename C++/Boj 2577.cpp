c3cdcd1dcae4bed16cc51d26fdb47ffc6509d1d8
Twince <tjsdn811@gmail.com>
2020-11-19 20:51:09 +0900

    algoritm

---
+++ C++/Boj 2577.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+int main(){
+	int A = 0, B = 0, C = 0;
+	int i, j, temp;
+	
+	string calc;
+	
+	cin >> A;
+	cin >> B;
+	cin >> C;
+	
+	temp = A*B*C;
+	calc = to_string(temp);
+
+	int array[10] = { 0,};
+	
+	for(i=0; i<calc.length(); i++){
+		for(j=0; j<10; j++){
+			if(calc[i] == (char)j+48)
+				array[j]++;
+		}
+	}
+	
+	for(i=0; i<10; i++)
+		cout << array[i] << '\n';
+	
+}
