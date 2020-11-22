1b66858ce3649fa6792615d3c44d7d0062b1c90d
Twince <tjsdn811@gmail.com>
2020-11-22 17:46:35 +0900

    C++

---
+++ C++/Boj 2440.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+	int i, j,input;
+	
+	cin >> input;
+	for(i=input; i>0; i--){
+		for(j=0; j<i; j++){
+			printf("*");
+		}	
+	printf("\n");
+	}
+	
+	
+}
